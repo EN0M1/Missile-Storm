6c12cfc6019a5f5f070963ae8d89b3401513a32b
EN0M1 <eli.mizerski@gmail.com>
2025-04-16 01:20:06 -0500

    Fixed Timer

--- Assets/Scipts/TimerBehavior.cs
+++ Assets/Scipts/TimerBehavior.cs
@@ -1,38 +1,46 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimerBehavior : MonoBehaviour
 {
-
     private float timer = 0.0f;
     private TextMeshProUGUI m_text;
+    private bool timerStarted = false;
+    private string targetSceneName = "GamePlay";
+    private float startTime;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_text = GetComponent<TextMeshProUGUI>();
-        Component[] cmps = GetComponents<Component>();
 
         if (m_text == null)
         {
-            Debug.Log("No TextMeshProUGU found");
+            Debug.Log("No TextMeshProUGUI found");
         }
+
+        timer = 0.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        timer = Time.time;
-
-        //Debug.Log("time thus far: " + timer);
-
-        if (m_text != null)
+        if (!timerStarted && SceneManager.GetActiveScene().name == targetSceneName)
         {
-            int minutes = Mathf.FloorToInt(timer / 60);
-            int second = Mathf.FloorToInt(timer % 60);
-            string timeLabel = string.Format("Time: {0:00}:{1:00}", minutes, second);
-            m_text.SetText(timeLabel);
+            startTime = Time.time;
+            timerStarted = true;
+        }
 
+        if (timerStarted)
+        {
+            timer = Time.time - startTime;
+
+            if (m_text != null)
+            {
+                int minutes = Mathf.FloorToInt(timer / 60);
+                int seconds = Mathf.FloorToInt(timer % 60);
+                string timeLabel = string.Format("Time: {0:00}:{1:00}", minutes, seconds);
+                m_text.SetText(timeLabel);
+            }
         }
     }
 }
